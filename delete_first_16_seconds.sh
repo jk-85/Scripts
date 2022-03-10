7e434471744a3c5f777c095cc8c60339657bdf77
jk-85 <jk-85@arcor.de>
2022-03-10 18:39:15 +0100

    Update delete_first_16_seconds.sh

--- delete_first_16_seconds.sh
+++ delete_first_16_seconds.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# Delete first 16 seconds of a video-file
+
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
-
+mkdir fertig
 for i in *.mp4;
 do
 filename=$(basename -- "$i")
@@ -10,7 +12,7 @@ filename="${filename%.*}"
 #echo $filename
 #ffmpeg -i $i -ss 16 -vn -c:a copy $filename.m4a
 #-vcodec copy -acodec copy
-ffmpeg -i $i -ss 16 -vcodec copy -acodec copy fertig$filename.mp4
+ffmpeg -i $i -ss 16 -vcodec copy -acodec copy fertig/$filename.mp4
 done
 
 IFS=$SAVEIFS
